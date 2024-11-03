3f5f4b5ad22506423334c1e974d2f2e30ded5e59
Juan Cespedes <juan.cespedes@imdea.org>
2024-11-03 15:53:20 +0100

    finished basic functionality and some tests

--- server.go
+++ server.go
@@ -1,13 +1,18 @@
 package api
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"reflect"
 )
 
 type Server struct {
+	debug    bool
+	mux      *http.ServeMux
+	patterns []string
 }
 
 type Request struct {
@@ -15,25 +20,47 @@ type Request struct {
 }
 
 func NewServer() *Server {
-	return nil
+	var s Server
+	s.mux = http.NewServeMux()
+	s.debug = false
+	return &s
 }
 
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Printf("new request: %v", r.URL)
+	if s.debug {
+		log.Printf("api.Server.ServeHTTP: new request: %v", r.URL)
+	}
+	s.mux.ServeHTTP(w, r)
 }
 
 // Handle registers a handle in the server.
 //
-// fun must be a function with one of these signatures:
+// handler must be a function with one of these signatures:
 //   - func [Input, Output any] (*Request, Input) (Output, error)
 //   - func [Output any] (*Request) (Output, error)
-func (s *Server) Handle(pattern string, fun any, permFuncs ...func(*Request) bool) {
-	t := reflect.TypeOf(fun)
+//   - func (http.ResponseWriter, *http.Request)
+func (s *Server) Handle(pattern string, handler any, permFuncs ...func(*Request) bool) {
+	if s == nil {
+		panic("api.Handle: called with nil Server")
+	}
+	s.patterns = append(s.patterns, pattern)
+	if handler == nil {
+		panic("api.Handle: called with nil handler")
+	}
+	t := reflect.TypeOf(handler)
 	if t == nil || t.Kind() != reflect.Func {
 		panic("api.Handle: Second argument must be a function")
 	}
 	if t.NumIn() < 1 || t.NumIn() > 2 {
 		panic("api.Handle: function must have 1 or 2 arguments")
+	}
+	v := reflect.ValueOf(handler)
+	if v.IsZero() {
+		panic("api.Handle: Second argument must be a non-nil function")
+	}
+	if f, ok := handler.(func(http.ResponseWriter, *http.Request)); ok {
+		s.mux.HandleFunc(pattern, f)
+		return
 	}
 	if t.In(0) != reflect.TypeOf(&Request{}) {
 		panic("api.Handle: first argument of function must have type *api.Request")
@@ -44,4 +71,164 @@ func (s *Server) Handle(pattern string, fun any, permFuncs ...func(*Request) boo
 	if t.Out(1) != reflect.TypeOf(errors.New).Out(0) {
 		panic("api.Handle: second return value of function must have type error")
 	}
+	nargs := t.NumIn()
+	var input any
+	if nargs == 2 {
+		input = reflect.New(t.In(1)).Interface()
+	}
+	// out := t.In(1)
+	s.mux.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
+		if s.debug {
+			log.Printf("calling handler for pattern %q", pattern)
+		}
+		var req *Request
+		var out []reflect.Value
+		if nargs == 1 {
+			out = v.Call([]reflect.Value{reflect.ValueOf(req)})
+		} else {
+			if r.ContentLength == 0 {
+				httpError(w, "no body supplied")
+				return
+			}
+			decoder := json.NewDecoder(r.Body)
+			decoder.DisallowUnknownFields()
+			if err := decoder.Decode(&input); err != nil {
+				httpError(w, "parsing body: %w", err)
+				return
+			}
+
+			out = v.Call([]reflect.Value{reflect.ValueOf(req), reflect.ValueOf(input).Elem()})
+		}
+		output := out[0].Interface()
+		var err error
+		if e := out[1].Interface(); e != nil {
+			err = out[1].Interface().(error)
+		}
+		if s.debug {
+			log.Printf("output from handler: (%v, %v)", output, err)
+		}
+		if err != nil {
+			httpError(w, err)
+			return
+		}
+
+		// if the returned type is a string, output it as a "info" message:
+		if s, ok := output.(string); ok {
+			httpInfo(w, s)
+			return
+		}
+
+		// if the returned type is a []byte, output it directly:
+		if b, ok := output.([]byte); ok {
+			w.Write(b)
+			return
+		}
+		httpJSON(w, output)
+	})
+	if s.debug {
+		log.Printf("Added new handler: pattern=%q func=%T", pattern, handler)
+	}
+}
+
+// Errors...:
+
+type errHTTPStatus struct {
+	Status int
+	Err    error
+}
+
+func (e errHTTPStatus) Error() string {
+	return e.Err.Error()
+}
+
+func (e errHTTPStatus) Unwrap() error {
+	return e.Err
+}
+
+// Error returns an ErrHTTPStatus from another error or a printf-like string.
+// The default HTTP status code is BadRequest.
+func apiError(f any, a ...any) error {
+	if err, ok := f.(errHTTPStatus); ok {
+		return err
+	}
+
+	var err error
+	if e, ok := f.(error); ok {
+		err = e
+	} else if s, ok := f.(string); ok {
+		err = fmt.Errorf(s, a...)
+	} else {
+		err = errors.New(fmt.Sprint(f))
+	}
+
+	code := http.StatusBadRequest
+	/*
+		switch {
+		case ErrorIsNotFound(err):
+			code = http.StatusNotFound
+			err = errors.New("not found")
+		}
+	*/
+	return CodeError(code, err)
+}
+
+// CodeError returns an error with an embedded HTTP status code
+func CodeError(code int, f any, a ...any) error {
+	var err error
+	if e, ok := f.(error); ok {
+		err = e
+	} else if s, ok := f.(string); ok && len(a) > 0 {
+		err = fmt.Errorf(s, a...)
+	} else {
+		err = errors.New(fmt.Sprint(f))
+	}
+
+	return errHTTPStatus{
+		Status: code,
+		Err:    err,
+	}
+}
+
+// httpError sends a HTTP error as a response
+func httpError(w http.ResponseWriter, f any, a ...any) {
+	err := apiError(f, a...).(errHTTPStatus)
+	/*
+		var perr *pq.Error
+		if errors.As(err, &perr) {
+			w.Header().Set("X-SQL-Error", fmt.Sprintf("%s %s", perr.Code, perr.Message))
+		}
+	*/
+	httpMessage(w, err.Status, "error", err.Error())
+}
+
+// httpError sends a HTTP error as a response
+func httpCodeError(w http.ResponseWriter, code int, f any, a ...any) {
+	err := CodeError(code, f, a...).(errHTTPStatus)
+	httpError(w, err)
+}
+
+func httpInfo(w http.ResponseWriter, msg any) {
+	httpMessage(w, http.StatusOK, "info", fmt.Sprint(msg))
+}
+
+func httpMessage(w http.ResponseWriter, code int, label string, msg string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	fmt.Fprintf(w, "{%q: %q}\n", label, msg)
+}
+
+func httpJSON(w http.ResponseWriter, output any, codes ...int) {
+	code := http.StatusOK
+	if len(codes) > 0 {
+		code = codes[0]
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	e := json.NewEncoder(w)
+	err := e.Encode(output)
+	if err != nil {
+		fmt.Fprintf(w, "{\"error\": %q}\n", err.Error())
+	}
+
 }
---
+++ server_test.go
@@ -0,0 +1,70 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestNewServer(t *testing.T) {
+	s := NewServer()
+	if s == nil {
+		t.Fatal("NewServer() returned nil")
+	}
+}
+
+func TestServerHandle(t *testing.T) {
+	s := NewServer()
+
+	testnum := 1
+	shouldPanic := func(h any) {
+		defer func() {
+			if recover() == nil {
+				t.Errorf("Handle() did not panic when handler's type is %T", h)
+			}
+		}()
+		s.Handle(fmt.Sprintf("/test.%d", testnum), h)
+		testnum++
+	}
+	shouldNotPanic := func(h any) {
+		defer func() {
+			if x := recover(); x != nil {
+				t.Errorf("Handle() panic'ed when it should not have: %v", x)
+			}
+		}()
+		s.Handle(fmt.Sprintf("/test.%d", testnum), h)
+		testnum++
+	}
+
+	// The second argument to Handle (the handler) should be a function:
+	shouldPanic(3)
+	shouldPanic("")
+	// Function must not be nil:
+	var f func(*Request) (any, error)
+	shouldPanic(f)
+	// If not an ordinary HYP handler, the function should have 1 or 2 input arguments:
+	shouldPanic(func() {})
+	shouldPanic(func() (string, error) { return "", nil })
+	shouldPanic(func(*Request, int, int) {})
+	shouldPanic(func(*Request, int, int) (string, error) { return "", nil })
+	// The first argument must be a *Request:
+	shouldPanic(func(int) (string, error) { return "", nil })
+	shouldPanic(func(string) (string, error) { return "", nil })
+	// There must be 2 return values:
+	shouldPanic(func(*Request) int { return 0 })
+	shouldPanic(func(*Request, int) int { return 0 })
+	shouldPanic(func(*Request) (int, error, int) { return 0, nil, 0 })
+	shouldPanic(func(*Request, int) (int, error, int) { return 0, nil, 0 })
+	// Second return value must be error:
+	shouldPanic(func(*Request) (int, int) { return 0, 0 })
+	shouldPanic(func(*Request, int) (int, int) { return 0, 0 })
+	shouldPanic(func(*Request) (int, string) { return 0, "" })
+	shouldPanic(func(*Request, int) (int, string) { return 0, "" })
+	shouldPanic(func(*Request) (int, any) { return 0, nil })
+	shouldPanic(func(*Request, int) (int, any) { return 0, nil })
+
+	// These should not panic:
+	shouldNotPanic(func(w http.ResponseWriter, r *http.Request) {}) // ordinary HTTP handler
+	shouldNotPanic(func(*Request, any) (any, error) { return nil, nil })
+	shouldNotPanic(func(*Request) (any, error) { return nil, nil })
+}
