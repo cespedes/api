c9fb85af052a5b1c794555de168b655e5016bc00
Juan Cespedes <juan.cespedes@imdea.org>
2026-02-23 20:58:16 +0100

    Added some tests (33.8% coverage)

--- server_test.go
+++ server_test.go
@@ -1,8 +1,11 @@
 package api
 
 import (
+	"bytes"
 	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func TestNewServer(t *testing.T) {
@@ -12,7 +15,7 @@ func TestNewServer(t *testing.T) {
 	}
 }
 
-func TestHandler(t *testing.T) {
+func TestCheckHandler(t *testing.T) {
 	s := NewServer()
 	if s == nil {
 		t.Fatal("NewServer() returned nil")
@@ -35,12 +38,13 @@ func TestHandler(t *testing.T) {
 	}
 
 	// The second argument to Handle (the handler) should be a function:
+	shouldPanic(nil)
 	shouldPanic(3)
 	shouldPanic("")
 	// Function must not be nil:
 	var f func(*http.Request) (any, error)
 	shouldPanic(f)
-	// If not an ordinary HYP handler, the function should have 1 or 2 input arguments:
+	// If not an ordinary HTTP handler, the function should have 1 or 2 input arguments:
 	shouldPanic(func() {})
 	shouldPanic(func() (string, error) { return "", nil })
 	shouldPanic(func(*http.Request, int, int) {})
@@ -62,7 +66,66 @@ func TestHandler(t *testing.T) {
 	shouldPanic(func(*http.Request, int) (int, any) { return 0, nil })
 
 	// These should not panic:
-	shouldNotPanic(func(w http.ResponseWriter, r *http.Request) {}) // ordinary HTTP handler
+	shouldNotPanic(http.NotFoundHandler())                          // ordinary HTTP handler
+	shouldNotPanic(func(w http.ResponseWriter, r *http.Request) {}) // ordinary HTTP handler func
 	shouldNotPanic(func(*http.Request, any) (any, error) { return nil, nil })
 	shouldNotPanic(func(*http.Request) (any, error) { return nil, nil })
+}
+
+func TestInputFields(t *testing.T) {
+	s := NewServer()
+	s.Handle("/foo", func(r *http.Request, in struct {
+		I int
+		S string
+	}) (any, error) {
+		m := InputFields(r)
+		if in.I != int(m["i"].(float64)) {
+			t.Fatalf("in.I=%d m[i]=%v\n", in.I, m["i"])
+		}
+		if in.S != m["s"] {
+			t.Fatalf("in.S=%s m[s]=%v\n", in.S, m["s"])
+		}
+		return nil, nil
+	})
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/foo", bytes.NewReader([]byte(`{"i":23,"s":"string"}`)))
+	s.ServeHTTP(w, r)
+}
+
+func TestServer(t *testing.T) {
+	s := NewServer()
+	s.Handle("/foo", func(r *http.Request) (any, error) {
+		t.Fatal("function called but perm function failed")
+		return nil, nil
+	}, func(r *http.Request) bool {
+		return false
+	})
+	bar := false
+	s.Handle("/bar", func(r *http.Request) (any, error) {
+		bar = true
+		return nil, nil
+	}, func(r *http.Request) bool {
+		return true
+	})
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/foo", nil)
+	s.ServeHTTP(w, r)
+
+	w = httptest.NewRecorder()
+	r = httptest.NewRequest("GET", "/bar", nil)
+	s.ServeHTTP(w, r)
+	time.Sleep(time.Second)
+	if !bar {
+		t.Fatal("function not called when perm function succeeded")
+	}
+
+	s.SetDenyHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		bar = false
+	}))
+	w = httptest.NewRecorder()
+	r = httptest.NewRequest("GET", "/foo", nil)
+	s.ServeHTTP(w, r)
+	if bar {
+		t.Fatal("denyHandler was not called")
+	}
 }
